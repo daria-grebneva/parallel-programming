6c011fea803241deb6d3cb87a22a8b5fa21cc1da
daria-grebneva <greda2598@gmail.com>
2019-06-01 20:02:15 +0300

    Правки по коду

--- lw4/CurrencySaver/ViewModels/ReceiverForm.Designer.cs
+++ lw4/CurrencySaver/ViewModels/ReceiverForm.Designer.cs
@@ -28,56 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this._currencyNamesUriLabel = new System.Windows.Forms.Label();
-            this._currencyInfosUriLabel = new System.Windows.Forms.Label();
-            this._currencyNamesUriBox = new System.Windows.Forms.TextBox();
-            this._currencyInfosUriBox = new System.Windows.Forms.TextBox();
+            this.inputFileLabel = new System.Windows.Forms.Label();
+            this.outputFileLabel = new System.Windows.Forms.Label();
+            this.inputFileBox = new System.Windows.Forms.TextBox();
+            this.outputFileBox = new System.Windows.Forms.TextBox();
             this._avgUpdateTimeDescriptionLabel = new System.Windows.Forms.Label();
-            this._avgUpdateTimeLabel = new System.Windows.Forms.Label();
-            this._asyncUsingRadioButton = new System.Windows.Forms.RadioButton();
-            this._syncUsingRadioButton = new System.Windows.Forms.RadioButton();
-            this._saveCurrenciesButton = new System.Windows.Forms.Button();
-            this._updateTimesList = new System.Windows.Forms.ListBox();
+            this.asyncRadioButton = new System.Windows.Forms.RadioButton();
+            this.syncRadioButton = new System.Windows.Forms.RadioButton();
+            this.processButton = new System.Windows.Forms.Button();
+            this.timesList = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // _currencyNamesUriLabel
+            // inputFileLabel
             // 
-            this._currencyNamesUriLabel.AutoSize = true;
-            this._currencyNamesUriLabel.Location = new System.Drawing.Point(16, 21);
-            this._currencyNamesUriLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this._currencyNamesUriLabel.Name = "_currencyNamesUriLabel";
-            this._currencyNamesUriLabel.Size = new System.Drawing.Size(115, 13);
-            this._currencyNamesUriLabel.TabIndex = 0;
-            this._currencyNamesUriLabel.Text = "Выберите input-файл:";
+            this.inputFileLabel.AutoSize = true;
+            this.inputFileLabel.Location = new System.Drawing.Point(16, 21);
+            this.inputFileLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.inputFileLabel.Name = "inputFileLabel";
+            this.inputFileLabel.Size = new System.Drawing.Size(115, 13);
+            this.inputFileLabel.TabIndex = 0;
+            this.inputFileLabel.Text = "Выберите input-файл:";
             // 
-            // _currencyInfosUriLabel
+            // outputFileLabel
             // 
-            this._currencyInfosUriLabel.AutoSize = true;
-            this._currencyInfosUriLabel.Location = new System.Drawing.Point(232, 21);
-            this._currencyInfosUriLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this._currencyInfosUriLabel.Name = "_currencyInfosUriLabel";
-            this._currencyInfosUriLabel.Size = new System.Drawing.Size(122, 13);
-            this._currencyInfosUriLabel.TabIndex = 1;
-            this._currencyInfosUriLabel.Text = "Выберите output-файл:";
+            this.outputFileLabel.AutoSize = true;
+            this.outputFileLabel.Location = new System.Drawing.Point(232, 21);
+            this.outputFileLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputFileLabel.Name = "outputFileLabel";
+            this.outputFileLabel.Size = new System.Drawing.Size(122, 13);
+            this.outputFileLabel.TabIndex = 1;
+            this.outputFileLabel.Text = "Выберите output-файл:";
             // 
-            // _currencyNamesUriBox
+            // inputFileBox
             // 
-            this._currencyNamesUriBox.Location = new System.Drawing.Point(18, 41);
-            this._currencyNamesUriBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this._currencyNamesUriBox.Name = "_currencyNamesUriBox";
-            this._currencyNamesUriBox.Size = new System.Drawing.Size(202, 20);
-            this._currencyNamesUriBox.TabIndex = 2;
+            this.inputFileBox.Location = new System.Drawing.Point(18, 41);
+            this.inputFileBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputFileBox.Name = "inputFileBox";
+            this.inputFileBox.Size = new System.Drawing.Size(202, 20);
+            this.inputFileBox.TabIndex = 2;
             // 
-            // _currencyInfosUriBox
+            // outputFileBox
             // 
-            this._currencyInfosUriBox.Location = new System.Drawing.Point(234, 41);
-            this._currencyInfosUriBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this._currencyInfosUriBox.Name = "_currencyInfosUriBox";
-            this._currencyInfosUriBox.Size = new System.Drawing.Size(202, 20);
-            this._currencyInfosUriBox.TabIndex = 3;
+            this.outputFileBox.Location = new System.Drawing.Point(234, 41);
+            this.outputFileBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.outputFileBox.Name = "outputFileBox";
+            this.outputFileBox.Size = new System.Drawing.Size(202, 20);
+            this.outputFileBox.TabIndex = 3;
             // 
             // _avgUpdateTimeDescriptionLabel
             // 
@@ -89,61 +89,61 @@
             this._avgUpdateTimeDescriptionLabel.TabIndex = 4;
             this._avgUpdateTimeDescriptionLabel.Text = "Среднее время выполнения: ";
             // 
-            // _avgUpdateTimeLabel
+            // label2
             // 
-            this._avgUpdateTimeLabel.AutoSize = true;
-            this._avgUpdateTimeLabel.Location = new System.Drawing.Point(393, 170);
-            this._avgUpdateTimeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this._avgUpdateTimeLabel.Name = "_avgUpdateTimeLabel";
-            this._avgUpdateTimeLabel.Size = new System.Drawing.Size(13, 13);
-            this._avgUpdateTimeLabel.TabIndex = 5;
-            this._avgUpdateTimeLabel.Text = "0";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 170);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
             // 
-            // _asyncUsingRadioButton
+            // asyncRadioButton
             // 
-            this._asyncUsingRadioButton.AutoSize = true;
-            this._asyncUsingRadioButton.Location = new System.Drawing.Point(19, 108);
-            this._asyncUsingRadioButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this._asyncUsingRadioButton.Name = "_asyncUsingRadioButton";
-            this._asyncUsingRadioButton.Size = new System.Drawing.Size(84, 17);
-            this._asyncUsingRadioButton.TabIndex = 7;
-            this._asyncUsingRadioButton.TabStop = true;
-            this._asyncUsingRadioButton.Text = "асинхронно";
-            this._asyncUsingRadioButton.UseVisualStyleBackColor = true;
-            this._asyncUsingRadioButton.CheckedChanged += new System.EventHandler(this._asyncUsingRadioButton_CheckedChanged);
+            this.asyncRadioButton.AutoSize = true;
+            this.asyncRadioButton.Location = new System.Drawing.Point(19, 108);
+            this.asyncRadioButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.asyncRadioButton.Name = "asyncRadioButton";
+            this.asyncRadioButton.Size = new System.Drawing.Size(84, 17);
+            this.asyncRadioButton.TabIndex = 7;
+            this.asyncRadioButton.TabStop = true;
+            this.asyncRadioButton.Text = "асинхронно";
+            this.asyncRadioButton.UseVisualStyleBackColor = true;
+            this.asyncRadioButton.CheckedChanged += new System.EventHandler(this._asyncUsingRadioButton_CheckedChanged);
             // 
-            // _syncUsingRadioButton
+            // syncRadioButton
             // 
-            this._syncUsingRadioButton.AutoSize = true;
-            this._syncUsingRadioButton.Location = new System.Drawing.Point(18, 134);
-            this._syncUsingRadioButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this._syncUsingRadioButton.Name = "_syncUsingRadioButton";
-            this._syncUsingRadioButton.Size = new System.Drawing.Size(78, 17);
-            this._syncUsingRadioButton.TabIndex = 8;
-            this._syncUsingRadioButton.TabStop = true;
-            this._syncUsingRadioButton.Text = "синхронно";
-            this._syncUsingRadioButton.UseVisualStyleBackColor = true;
-            this._syncUsingRadioButton.CheckedChanged += new System.EventHandler(this._syncUsingRadioButton_CheckedChanged);
+            this.syncRadioButton.AutoSize = true;
+            this.syncRadioButton.Location = new System.Drawing.Point(18, 134);
+            this.syncRadioButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.syncRadioButton.Name = "syncRadioButton";
+            this.syncRadioButton.Size = new System.Drawing.Size(78, 17);
+            this.syncRadioButton.TabIndex = 8;
+            this.syncRadioButton.TabStop = true;
+            this.syncRadioButton.Text = "синхронно";
+            this.syncRadioButton.UseVisualStyleBackColor = true;
+            this.syncRadioButton.CheckedChanged += new System.EventHandler(this._syncUsingRadioButton_CheckedChanged);
             // 
-            // _saveCurrenciesButton
+            // processButton
             // 
-            this._saveCurrenciesButton.Location = new System.Drawing.Point(316, 128);
-            this._saveCurrenciesButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this._saveCurrenciesButton.Name = "_saveCurrenciesButton";
-            this._saveCurrenciesButton.Size = new System.Drawing.Size(119, 29);
-            this._saveCurrenciesButton.TabIndex = 9;
-            this._saveCurrenciesButton.Text = "обработать";
-            this._saveCurrenciesButton.UseVisualStyleBackColor = true;
-            this._saveCurrenciesButton.Click += new System.EventHandler(this._saveCurrenciesButton_Click_1);
+            this.processButton.Location = new System.Drawing.Point(316, 128);
+            this.processButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(119, 29);
+            this.processButton.TabIndex = 9;
+            this.processButton.Text = "обработать";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this._saveCurrenciesButton_Click_1);
             // 
-            // _updateTimesList
+            // timesList
             // 
-            this._updateTimesList.FormattingEnabled = true;
-            this._updateTimesList.Location = new System.Drawing.Point(18, 201);
-            this._updateTimesList.Margin = new System.Windows.Forms.Padding(2);
-            this._updateTimesList.Name = "_updateTimesList";
-            this._updateTimesList.Size = new System.Drawing.Size(418, 160);
-            this._updateTimesList.TabIndex = 0;
+            this.timesList.FormattingEnabled = true;
+            this.timesList.Location = new System.Drawing.Point(18, 201);
+            this.timesList.Margin = new System.Windows.Forms.Padding(2);
+            this.timesList.Name = "timesList";
+            this.timesList.Size = new System.Drawing.Size(418, 160);
+            this.timesList.TabIndex = 0;
             // 
             // label1
             // 
@@ -164,33 +164,33 @@
             this.label4.TabIndex = 11;
             this.label4.Text = "Время выполнений";
             // 
-            // label7
+            // label3
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(414, 170);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(21, 13);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "мс";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "мс";
             // 
             // ReceiverForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(452, 375);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this._updateTimesList);
-            this.Controls.Add(this._saveCurrenciesButton);
-            this.Controls.Add(this._syncUsingRadioButton);
-            this.Controls.Add(this._asyncUsingRadioButton);
-            this.Controls.Add(this._avgUpdateTimeLabel);
+            this.Controls.Add(this.timesList);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.syncRadioButton);
+            this.Controls.Add(this.asyncRadioButton);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this._avgUpdateTimeDescriptionLabel);
-            this.Controls.Add(this._currencyInfosUriBox);
-            this.Controls.Add(this._currencyNamesUriBox);
-            this.Controls.Add(this._currencyInfosUriLabel);
-            this.Controls.Add(this._currencyNamesUriLabel);
+            this.Controls.Add(this.outputFileBox);
+            this.Controls.Add(this.inputFileBox);
+            this.Controls.Add(this.outputFileLabel);
+            this.Controls.Add(this.inputFileLabel);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ReceiverForm";
             this.Text = "CurrencyExchangeReceiver";
@@ -201,19 +201,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label _currencyNamesUriLabel;
-        private System.Windows.Forms.Label _currencyInfosUriLabel;
-        private System.Windows.Forms.TextBox _currencyNamesUriBox;
-        private System.Windows.Forms.TextBox _currencyInfosUriBox;
+        private System.Windows.Forms.Label inputFileLabel;
+        private System.Windows.Forms.Label outputFileLabel;
+        private System.Windows.Forms.TextBox inputFileBox;
+        private System.Windows.Forms.TextBox outputFileBox;
         private System.Windows.Forms.Label _avgUpdateTimeDescriptionLabel;
-        private System.Windows.Forms.Label _avgUpdateTimeLabel;
-        private System.Windows.Forms.RadioButton _asyncUsingRadioButton;
-        private System.Windows.Forms.RadioButton _syncUsingRadioButton;
-        private System.Windows.Forms.Button _saveCurrenciesButton;
-        private System.Windows.Forms.ListBox _updateTimesList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton asyncRadioButton;
+        private System.Windows.Forms.RadioButton syncRadioButton;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.ListBox timesList;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- lw4/CurrencySaver/ViewModels/ReceiverForm.cs
+++ lw4/CurrencySaver/ViewModels/ReceiverForm.cs
@@ -12,37 +12,37 @@ namespace CurrencyExchangeReceiver
         {
             InitializeComponent();
             _viewModel = mainFormViewModel;
-            _updateTimesList.DataBindings.Add( "DataSource", _viewModel, "UpdateTimes", true, DataSourceUpdateMode.OnPropertyChanged );
-            _avgUpdateTimeLabel.DataBindings.Add( "Text", _viewModel, "AvgUpdateTime", true, DataSourceUpdateMode.OnPropertyChanged );
-            _asyncUsingRadioButton.Checked = true;
+            timesList.DataBindings.Add( "DataSource", _viewModel, "UpdateTimes", true, DataSourceUpdateMode.OnPropertyChanged );
+            label2.DataBindings.Add( "Text", _viewModel, "AvgUpdateTime", true, DataSourceUpdateMode.OnPropertyChanged );
+            asyncRadioButton.Checked = true;
         }
 
         private void _asyncUsingRadioButton_CheckedChanged( object sender, System.EventArgs e )
         {
-            _saveCurrenciesButton.Click -= _saveCurrenciesButton_ClickAsync;
-            _saveCurrenciesButton.Click -= _saveCurrenciesButton_Click;
-            _saveCurrenciesButton.Click += new EventHandler( _saveCurrenciesButton_ClickAsync );
+            processButton.Click -= _saveCurrenciesButton_ClickAsync;
+            processButton.Click -= _saveCurrenciesButton_Click;
+            processButton.Click += new EventHandler( _saveCurrenciesButton_ClickAsync );
         }
 
         private void _syncUsingRadioButton_CheckedChanged( object sender, EventArgs e )
         {
-            _saveCurrenciesButton.Click -= _saveCurrenciesButton_ClickAsync;
-            _saveCurrenciesButton.Click -= _saveCurrenciesButton_Click;
-            _saveCurrenciesButton.Click += new EventHandler( _saveCurrenciesButton_Click );
+            processButton.Click -= _saveCurrenciesButton_ClickAsync;
+            processButton.Click -= _saveCurrenciesButton_Click;
+            processButton.Click += new EventHandler( _saveCurrenciesButton_Click );
         }
 
         private async void _saveCurrenciesButton_ClickAsync( object sender, EventArgs e )
         {
-            _saveCurrenciesButton.Enabled = false;
-            await _viewModel.SaveCurrencyInfosAsync( _currencyNamesUriBox.Text, _currencyInfosUriBox.Text );
-            _saveCurrenciesButton.Enabled = true;
+            processButton.Enabled = false;
+            await _viewModel.SaveCurrencyInfosAsync( inputFileBox.Text, outputFileBox.Text );
+            processButton.Enabled = true;
         }
 
         private void _saveCurrenciesButton_Click( object sender, EventArgs e )
         {
-            _saveCurrenciesButton.Enabled = false;
-            _viewModel.SaveCurrencyInfos( _currencyNamesUriBox.Text, _currencyInfosUriBox.Text );
-            _saveCurrenciesButton.Enabled = true;
+            processButton.Enabled = false;
+            _viewModel.SaveCurrencyInfos( inputFileBox.Text, outputFileBox.Text );
+            processButton.Enabled = true;
         }
 
         private void _saveCurrenciesButton_Click_1(object sender, EventArgs e)
